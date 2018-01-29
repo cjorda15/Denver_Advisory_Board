4be9ddf66b163f07774a2bbf925d58ec45a77ab4
Chris Jordan <crobertjordan@yahoo.com>
2018-01-28 20:40:30 -0700

    heroku attempt

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -2,7 +2,12 @@ import React from 'react';
 import './home.scss';
 
 const Home = () => {
-  return <div id="home-container" />;
+  return (
+    <div
+      id="home-container"
+      style={{ backgroundImage: "url('codeSport.png') " }}
+    />
+  );
 };
 
 export default Home;
