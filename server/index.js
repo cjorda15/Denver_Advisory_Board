3471760b0ef97bf318f02c5e797794f47486101e
Chris Jordan <crobertjordan@yahoo.com>
2018-01-31 18:16:41 -0700

    have mongo and mongoose setup

--- server/index.js
+++ server/index.js
@@ -5,6 +5,11 @@ const bodyParser = require('body-parser');
 const cloudinary = require('cloudinary');
 const apiRoutes = require('./routes.js');
 const config = require('./config').app;
+const MongoClient = require('mongodb').MongoClient;
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const mongoURL = process.env.MONGODB_URL || process.env.localMongo;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -19,6 +24,39 @@ app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, '../public/index.html'));
 });
 
-app.listen(config.port, () => {
-  console.log('we are live..');
+var Schema = mongoose.Schema;
+
+var Users = new Schema(
+  {
+    name: String,
+    age: Number
+  },
+  {
+    collection: 'collectionName'
+  }
+);
+
+mongoose.connect(mongoURL);
+
+let db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log(db, '!!');
+  console.log('connected');
+});
+//
+
+var Users = mongoose.model('Users', Users);
+
+var Chris = new Users({
+  name: 'ganadsfasdfnon',
+  age: 202304023940,
+  fat: true
+});
+
+Chris.save(function(error) {
+  console.log('SAVED');
+  if (error) {
+    console.error(error);
+  }
 });
