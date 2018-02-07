402356ba25a53818aa4624e248e46c55d48da85d
Chris Jordan <crobertjordan@yahoo.com>
2018-02-06 18:35:42 -0700

    got the x animation, needs some touch up and hover effect for close button as well

--- src/components/Events/events.js
+++ src/components/Events/events.js
@@ -24,4 +24,25 @@ $(document).ready(function() {
       .find('.open-event-click-line-3')
       .toggleClass('open-event-line-3-hover');
   });
+
+  $(document).on('click', '.open-event-click-open-btn', function() {
+    // $(this)
+    //   .find('.open-event-click-line-1')
+    //   .toggleClass('open-event-line-1-hover');
+    // $(this)
+    //   .find('.open-event-click-line-2')
+    //   .toggleClass('open-event-line-2-hover');
+    // $(this)
+    //   .find('.open-event-click-line-3')
+    //   .toggleClass('open-event-line-3-hover');
+    $(this)
+      .find('.open-event-click-line-1')
+      .toggleClass('close-event-line-1');
+    $(this)
+      .find('.open-event-click-line-2')
+      .toggleClass('close-event-line-2');
+    $(this)
+      .find('.open-event-click-line-3')
+      .toggleClass('close-event-line-3');
+  });
 });
